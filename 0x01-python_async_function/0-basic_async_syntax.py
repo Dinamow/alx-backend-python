d810093c86f3681f9409dc19b9b336a1a8fbbcc3
DINAMOW <meemoo102039@gmail.com>
2024-01-15 11:52:22 +0200

    Variable Annotations

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python3
 import asyncio
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """Asynchronous coroutine that takes in an integer argument
     Args:
         max_delay (int, optional): [description]. Defaults to 10.
@@ -9,6 +8,6 @@ async def wait_random(max_delay = 10):
         float: random delay between 0 and max_delay
     """
     import random
-    delay = random.uniform(0, max_delay)
+    delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
