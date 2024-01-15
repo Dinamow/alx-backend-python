5a67ab90a57f519b842abedb6c358cab223e990c
DINAMOW <meemoo102039@gmail.com>
2024-01-15 10:31:03 +0200

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+import asyncio
+
+async def wait_random(max_delay = 10):
+    """Asynchronous coroutine that takes in an integer argument
+    Args:
+        max_delay (int, optional): [description]. Defaults to 10.
+    Returns:
+        float: random delay between 0 and max_delay
+    """
+    import random
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
