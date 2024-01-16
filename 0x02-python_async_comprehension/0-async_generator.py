e14c56e0893bfd90213585e465936d50ae63abc1
DINAMOW <meemoo102039@gmail.com>
2024-01-16 11:17:15 +0200

    asynco.sleep

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """async_generator module file for 0x02-python_async_comprehension project"""
-from typing import AsyncGenerator
+from typing import Generator
+import asyncio
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None]:
     """
     async_generator function
     Returns:
@@ -11,4 +12,5 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """
     import random
     for i in range(10):
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
