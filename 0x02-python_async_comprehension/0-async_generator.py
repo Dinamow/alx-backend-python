02603a565a0874fed247e619c393e54ea727c360
DINAMOW <meemoo102039@gmail.com>
2024-01-16 11:15:14 +0200

    change return to AsyncGenerator"

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """async_generator module file for 0x02-python_async_comprehension project"""
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     async_generator function
     Returns:
