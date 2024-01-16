dbd4e6ad4d22948f9a7e59119fafbe0ed96a89d2
DINAMOW <meemoo102039@gmail.com>
2024-01-16 09:33:29 +0200

    make it type-annotated

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 """async_generator module file for 0x02-python_async_comprehension project"""
+from typing import Generator
 
 
-async def async_generator():
-    """async_generator function"""
+async def async_generator() -> Generator[float, None, None]:
+    """
+    async_generator function
+    Returns:
+        Generator[float, None, None]: [description]
+    """
     import random
     for i in range(10):
         yield random.uniform(0, 10)
