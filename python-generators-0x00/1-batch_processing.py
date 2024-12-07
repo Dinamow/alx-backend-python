dcaa904b614d60c3dde537950940c2acbe0d6941
DINAMOW <meemoo102039@gmail.com>
2024-12-07 16:03:24 +0200

    task 2

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,41 @@
+import mysql.connector
+
+def stream_users_in_batches(batch_size):
+    """
+    Streams user data from the ALX_prodev database in batches.
+
+    Args:
+        batch_size (int): The number of rows of user data to include in each batch.
+
+    Yields:
+        list of dict: A list of dictionaries, each representing a row of user data.
+    """
+    try:
+        with mysql.connector.connect(
+            host='localhost',
+            user='dinamow',
+            password='password',
+            database='ALX_prodev'
+        ) as connection:
+            with connection.cursor(dictionary=True) as cursor:
+                cursor.execute(f"SELECT * FROM user_data ORDER BY id LIMIT {batch_size};")
+                while True:
+                    batch = cursor.fetchone()
+                    if not batch:
+                        break
+                    yield batch
+    except mysql.connector.Error as err:
+        print(f"Database error: {err}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
+def batch_processing(batch_size):
+    """
+    Processes and prints batches of user data from the ALX_prodev database.
+
+    Args:
+        batch_size (int): The number of rows of user data to include in each batch.
+    """
+    for batch in stream_users_in_batches(batch_size):
+        if batch['age'] > 25:
+            print(batch)
